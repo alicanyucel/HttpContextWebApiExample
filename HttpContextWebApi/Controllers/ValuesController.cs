faa5f387a298154e8bb31e82b87cf1801e76f8ab
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2024-05-13 20:56:24 +0300

    HttpContextWebApi

---
+++ HttpContextWebApi/Controllers/ValuesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HttpContextWebApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public sealed class ValuesController : ControllerBase
+    {
+        [HttpGet] 
+        public IActionResult Get(int age,string name)
+        {
+            var httpcontext = HttpContext;
+            return NoContent(); // herhangi bir cevap dönmüyor
+        }
+    }
+}
